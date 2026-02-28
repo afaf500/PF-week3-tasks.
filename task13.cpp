f0c5a0366030e2300cfa26f7e18ea87335df275c
afaf500 <afafali7323@gmail.com>
2026-02-28 06:15:06 +0500

    Add files via upload

---
+++ task13.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+main()
+{
+int num1,num2,num3,num4,num5;
+int sum;
+
+cout<<"Enter number 1:";
+cin>>num1;
+
+cout<<"Enter number 2 :";
+cin>>num2;
+
+cout<<"Enter number 3:";
+cin>>num3;
+
+cout<<"Enter number 4:";
+cin>>num4;
+
+cout<<"Enter number 5:";
+cin>>num5;
+
+sum=num1+num2+num3+num4+num5;
+
+cout<<"Sum of numbers is:"<<sum;
+}
+
+
