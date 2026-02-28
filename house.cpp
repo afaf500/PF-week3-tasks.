63f20e8526fb05e178dde588a8475e2c28d45829
afaf500 <afafali7323@gmail.com>
2026-02-28 06:08:16 +0500

    Add files via upload

---
+++ house.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int age;
+int moves;
+int average;
+int house;
+
+cout<<"Enter age:"<<endl;
+cin>>age;
+
+cout<<"Enter moves:"<<endl;
+cin>>moves;
+
+house=moves+1;
+average=age/house;
+
+cout<<"average years are :"<<average;
+}
+
