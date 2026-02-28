9dc764a785c183abae5adf3bfe6ba4cf30115271
afaf500 <afafali7323@gmail.com>
2026-02-28 06:04:32 +0500

    Add files via upload

---
+++ aggregate.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main()
+{
+string Name;
+int roll_num;
+char section;
+float aggregate;
+cout<<" Enter your Name:" <<endl;
+cin>> Name;
+cout<< " Enter your roll num:" <<endl;
+cin>> roll_num ;
+cout<< "Enter your section:" <<endl;
+cin>> section;
+cout << "Enter your aggregate:" <<endl;
+cin>> aggregate;
+
+cout<<"-----------------------------------------"<<endl;
+cout<<"Name:"<<Name<<endl;
+cout<<"Roll Num:"<<roll_num<<endl;
+cout<<"Aggregate:"<<aggregate<<endl;
+cout<<"Section:"<<section<<endl;
+}
+
+
+
