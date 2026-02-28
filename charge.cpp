5a060857855482035875f84e688c15e5895f38c0
afaf500 <afafali7323@gmail.com>
2026-02-28 06:05:51 +0500

    Add files via upload

---
+++ charge.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main()
+{
+float Q;
+float t;
+float A;
+cout<<"Enter charge in coloumbs:"<<endl;
+cin>>Q;
+cout <<"Enter time in seconds:"<<endl;
+cin>>t;
+A=Q/t;
+cout<<"Current in Amperes"<<A;
+}
+
