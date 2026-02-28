111328a0c42c5453990487f062192ff41163f233
afaf500 <afafali7323@gmail.com>
2026-02-28 06:07:53 +0500

    Add files via upload

---
+++ hours.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+float hours;
+float sec;
+cout<<"Enter the number of hours"<<endl;
+cin>>hours;
+sec=hours*60*60;
+cout<<hours<<"is equal to:"<<sec<<"sec"<<endl;
+}
