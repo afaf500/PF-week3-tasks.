56cac8446575e9426e4b5647e7cd9ba6d7979804
afaf500 <afafali7323@gmail.com>
2026-02-28 06:12:45 +0500

    Add files via upload

---
+++ polygon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main()
+{
+int sides;
+cout<<"Enter sides of polygon:"<<endl;
+cin>>sides;
+
+int degrees;
+degrees=(sides-2)*180;
+
+cout<<"sum of angles is:"<<degrees;
+}
+
