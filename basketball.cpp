9842f44ca1e8ca9f8582b85e720bb436b4455448
afaf500 <afafali7323@gmail.com>
2026-02-28 06:05:00 +0500

    Add files via upload

---
+++ basketball.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int pointer2;
+int pointer3;
+
+cout<<"Enter 2 pointer :"<<endl;
+cin>>pointer2;
+
+cout<<"Enter 3 pointer :"<<endl;
+cin>>pointer3;
+
+int total;
+total=(pointer2 * 2)+(pointer3 * 3);
+
+cout<<"Total points are:"<<total;
+}
