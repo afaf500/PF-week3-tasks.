c5445e4e662503ac54e6bc1af363e6edf1732f7f
afaf500 <afafali7323@gmail.com>
2026-02-28 06:14:35 +0500

    Add files via upload

---
+++ rectangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main()
+{
+int Length;
+int Width;
+int Area;
+cout<<"Length of rectangle:"<<endl;
+cin>>Length;
+cout<<"Width of rectangle:"<<endl;
+cin>>Width;
+Area=Length*Width;
+cout<<"Area of rectangle is:"<<Area;
+}
