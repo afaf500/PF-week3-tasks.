e435d06d609030cbbe3aed39af5a7ae25a8aa014
afaf500 <afafali7323@gmail.com>
2026-02-28 06:17:05 +0500

    Add files via upload

---
+++ weight.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+float Pounds;
+int Kilogram;
+cout<< " Enter weight in Pounds" <<endl;
+cin>> Pounds;
+Kilogram=Pounds*0.45;
+cout<<"Weight in kilogram is"<<Kilogram<< "kg";
+}
